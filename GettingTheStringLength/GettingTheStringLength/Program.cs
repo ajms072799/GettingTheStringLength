280c74af4271bfb3742d19242ca75f92f8b960c6
Aron James delos Reyes <ajms.072799@gmail.com>
2020-10-20 19:53:01 +0800

    Adding a method for getting the string input

--- GettingTheStringLength/GettingTheStringLength/Program.cs
+++ GettingTheStringLength/GettingTheStringLength/Program.cs
@@ -10,6 +10,17 @@ namespace GettingTheStringLength
     {
         static void Main(string[] args)
         {
+           // Getting the string length
+
+            // <~ End of the program
+            Console.ReadLine();
+        }
+        static string GettingTheInputedString()
+        {
+            Console.Write("Enter your string: ");
+            string inputString = Console.ReadLine();
+
+            return inputString;
         }
     }
 }
